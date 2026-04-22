351b14d6a80d36b3f52699e08e34126069759ec7
yogeshingulkar <yogeshingulkar750@gmail.com>
2026-04-22 08:58:34 +0530

    updates

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Automation Exercise - Search & Dropdown', () => {
+describe('Automation Exercise - Search options & Dropdown options', () => {
 
   beforeEach(() => {
     cy.visit('https://automationexercise.com/')
@@ -9,86 +9,97 @@ describe('Automation Exercise - Search & Dropdown', () => {
   it('Search product and validate results', () => {
 
     cy.get('a[href="/products"]').click()
-
-    cy.get('#search_product').type('Tshirt')
+    cy.get('#search_product').clear().type('Tshirt').should('have.value', 'Tshirt')
     cy.get('#submit_search').click()
-
-    cy.get('.productinfo').should('exist')
+    cy.url().should('include', 'search')
+    cy.get('.productinfo').should('exist').and('be.visible').as('products')
+    cy.get('@products').its('length').should('be.greaterThan', 0)
 
   })
 
-  it('Validate search results contain keyword', () => {
+
+  it('Validate search results contain keyword (robust)', () => {
 
     cy.get('a[href="/products"]').click()
-
     cy.get('#search_product').type('Dress')
     cy.get('#submit_search').click()
 
-    cy.get('.productinfo').each(($el) => {
-      expect($el.text().toLowerCase()).to.include('dress')
+    cy.get('.productinfo').then(($products) => {
+      const matches = []
+      $products.each((index, el) => {
+        const text = el.innerText.toLowerCase()
+        if (text.includes('dress')) {
+          matches.push(text)
+        }
+      })
+      expect(matches.length).to.be.greaterThan(0)
     })
-
   })
 
-  it('Search with no results', () => {
+
+  it('Search with no results (correct validation)', () => {
 
     cy.get('a[href="/products"]').click()
-
     cy.get('#search_product').type('xyz123')
     cy.get('#submit_search').click()
-
-    cy.get('body').should('contain.text', 'No products')
+    cy.get('.productinfo').should('have.length', 0)
 
   })
 
-  it('Filter products by category (Dropdown-like)', () => {
+
+  it('Filter products by category (proper visibility handling)', () => {
 
     cy.get('a[href="/products"]').click()
+    cy.get('.panel-heading').first().should('be.visible').click()
 
-    cy.get('.panel-heading').first().click()
+    cy.get('#Women')
+      .should('be.visible')
+      .within(() => {
+        cy.get('ul li a').first().should('be.visible').click()
+      })
 
-    cy.get('.panel-body ul li a').first().click()
-
-    cy.get('.productinfo').should('exist')
+    cy.get('.productinfo').should('exist').and('have.length.greaterThan', 0)
 
   })
+
 
   it('Select sub-category and validate products', () => {
 
     cy.get('a[href="/products"]').click()
+    cy.get('.panel-heading').first().click()
+    cy.get('#Women')
+      .should('be.visible')
+      .within(() => {
+        cy.get('ul li a').eq(0).click()
+      })
 
-    cy.get('.panel-heading').eq(1).click()
-
-    cy.get('.panel-body ul li a').eq(0).click()
-
-    cy.get('.productinfo').should('have.length.greaterThan', 0)
+    cy.get('.productinfo').should('be.visible').and('have.length.greaterThan', 0)
 
   })
 
-  it('Search + Add to cart flow', () => {
+
+  it('Search + Add to cart flow (advanced)', () => {
 
     cy.get('a[href="/products"]').click()
-
     cy.get('#search_product').type('Top')
     cy.get('#submit_search').click()
 
-    cy.get('.product-overlay').first().invoke('show')
-    cy.contains('Add to cart').first().click({ force: true })
-
-    cy.get('#cartModal').should('be.visible')
-
+    cy.get('.productinfo').first().within(() => {
+        cy.get('.product-overlay').invoke('show')
+        cy.get('a').contains('Add to cart').click({ force: true })
+      })
+    cy.get('#cartModal').should('be.visible').and('contain.text', 'Added')
   })
 
-  it('Validate dropdown navigation flow', () => {
+  it('Validate dropdown navigation flow using each()', () => {
 
     cy.get('a[href="/products"]').click()
+    cy.get('.panel-heading').should('have.length.greaterThan', 1)
+      .each(($el, index) => {
 
-    cy.get('.panel-heading').each(($el, index) => {
-      if (index < 2) {
-        cy.wrap($el).click()
-      }
-    })
-
+        if (index < 2) {
+          cy.wrap($el).scrollIntoView().should('be.visible').click()
+        }
+      })
   })
-
 })
